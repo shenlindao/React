cf7d8f1b26160180c80dff4c80fffc831a0be45f
Artem <shenlin@yandex.ru>
2024-09-17 11:08:43 +0300

    Homework 2

---
+++ Homework2/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import CommentsList from './components/CommentsList';
+
+function App() {
+  return (
+    <CommentsList />
+  );
+}
+
+export default App;
---
+++ Homework2/src/components/CommentsList.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export function Comments() {
+    const [comments, setComments] = useState([
+        { id: 1, text: "Это первый комментарий" },
+        { id: 2, text: "Это второй комментарий" },
+        { id: 3, text: "Это третий комментарий" }
+    ]);
+
+    const handleDelete = (id) => {
+        setComments(comments.filter(comment => comment.id !== id));
+    };
+
+    return (
+        <div className="comments-container">
+            {comments.map((comment) => (
+                <div key={comment.id} className="comment-card">
+                    <p>{comment.text}</p>
+                    <button onClick={() => handleDelete(comment.id)} className="delete-btn">
+                        Удалить
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Comments;
